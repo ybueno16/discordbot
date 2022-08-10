2bc6ced7a756f4a25af119d736d456a797077ee9
Yuri Bueno <70299567+ybueno16@users.noreply.github.com>
2022-08-10 12:40:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -61,4 +61,4 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 
-client.run("OTIwMDMzNzcxODQyMDU2MTkz.GkUqEp.rZZvLePS1IXViNKeavGp3TRllSQ1TcrngcWHhg")
+client.run()
